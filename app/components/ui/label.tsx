d5ad4db2d9991c656d86f7ef7d044e176725d21d
Md Siam Maruf <siam.maruf1@gmail.com>
2025-05-01 23:50:15 +0600

    Update ts

--- app/components/ui/label.tsx
+++ app/components/ui/label.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import * as React from "react"
 import * as LabelPrimitive from "@radix-ui/react-label"
 
@@ -10,7 +8,7 @@ function Label({
   ...props
 }: React.ComponentProps<typeof LabelPrimitive.Root>) {
   return (
-    <LabelPrimitive.Root
+    <LabelPrimitive.Root as="label"
       data-slot="label"
       className={cn(
         "flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50",
